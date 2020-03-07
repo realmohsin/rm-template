5a4b47361e219417607bb6a00fd5372427c2472c
Real Mohsin <realmohsin@outlook.com>
2020-03-07 07:57:06 -0500

    logo, not found logo added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,11 @@ import { css } from '@emotion/core'
 const HomePage = props => {
   return (
     <div css={homePage}>
+      <img
+        alt='Logo'
+        src='https://image.flaticon.com/icons/svg/1147/1147083.svg'
+        css={logo}
+      />
       <h1>SPA Template</h1>
     </div>
   )
@@ -11,6 +16,11 @@ const HomePage = props => {
 
 const homePage = theme => css`
   text-align: center;
+`
+
+const logo = theme => css`
+  margin: 25px auto;
+  width: 100px;
 `
 
 export default HomePage
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,6 +4,11 @@ import { css } from '@emotion/core'
 const NotFound = props => {
   return (
     <div css={notFound}>
+      <img
+        alt='Not Found Logo'
+        src='https://image.flaticon.com/icons/svg/2471/2471920.svg'
+        css={notFoundLogo}
+      />
       <h1>Not Found</h1>
     </div>
   )
@@ -11,6 +16,11 @@ const NotFound = props => {
 
 const notFound = theme => css`
   text-align: center;
+`
+
+const notFoundLogo = theme => css`
+  width: 100px;
+  margin: 25px auto;
 `
 
 export default NotFound
